69cd0e64401aa8c85a33976b06ae58c2407a7578
keersbidare <kbida003@wolverine.cs.ucr.edu>
2024-04-11 19:37:10 -0700

    pragma unroll for dotmultiply,launching two kernels to check op with and without optimization

--- src/host.cpp
+++ src/host.cpp
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
    
-    clock_t htod, dtoh, comp;
+    clock_t htod, dtoh, comp, comp_opti,dtoh_opti,htod_opti;
 
 
     /*====================================================CL===============================================================*/
@@ -40,6 +40,7 @@ int main(int argc, char** argv) {
             std::cout << "Device[" << i << "]: program successful!\n";
             std::cout << "Setting CU(s) up..." << std::endl;
             OCL_CHECK(err, krnl1 = cl::Kernel(program, "krnl", &err));
+            OCL_CHECK(err, krnl2 = cl::Kernel(program, "krnl", &err));
             valid_device = true;
             break; // we break because we found a valid device
         }
@@ -52,13 +53,16 @@ int main(int argc, char** argv) {
 
     /*====================================================INIT INPUT/OUTPUT VECTORS===============================================================*/
     std::vector<data_t, aligned_allocator<data_t> > input(DATA_SIZE);
+    std::vector<data_t, aligned_allocator<data_t> > input_opti(DATA_SIZE);
     data_t *input_sw = (data_t*) malloc(sizeof(data_t) * (DATA_SIZE));
     std::vector<data_t, aligned_allocator<data_t> > output_hw(1);
+    std::vector<data_t, aligned_allocator<data_t> > output_hw_opti(1);
     data_t *output_sw = (data_t*) malloc(sizeof(data_t) * (1));
 
 
     for (int i = 0; i < DATA_SIZE; i++) {
         input[i] = data[i];
+        input_opti[i] = data[i];
         input_sw[i] = data[i];
     }
 
@@ -71,51 +75,86 @@ int main(int argc, char** argv) {
 
     /* INPUT BUFFERS */
     OCL_CHECK(err, cl::Buffer buffer_input(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(data_t) * DATA_SIZE, input.data(), &err));  
+    OCL_CHECK(err, cl::Buffer buffer_input_optimized(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(data_t) * DATA_SIZE, input.data(), &err));  
 
 
     /* OUTPUT BUFFERS */
     OCL_CHECK(err, cl::Buffer buffer_output(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(data_t) * 1, output_hw.data(), &err));
+    OCL_CHECK(err, cl::Buffer buffer_output_optimized(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(data_t) * 1, output_hw_opti.data(), &err));
 
 
     /* SETTING INPUT PARAMETERS */
     OCL_CHECK(err, err = krnl1.setArg(0, buffer_input));
     OCL_CHECK(err, err = krnl1.setArg(1, buffer_output));
+    OCL_CHECK(err, err = krnl1.setArg(2, false));
+
+    OCL_CHECK(err, err = krnl2.setArg(0, buffer_input_optimized));
+    OCL_CHECK(err, err = krnl2.setArg(1, buffer_output_optimized));
+    OCL_CHECK(err, err = krnl2.setArg(2, true));
 
     /*====================================================KERNEL===============================================================*/
     /* HOST -> DEVICE DATA TRANSFER*/
-    std::cout << "HOST -> DEVICE" << std::endl;
+    std::cout << "HOST -> DEVICE [Kernel-1]" << std::endl;
     htod = clock();
-    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_input}, 0 /* 0 means from host*/));
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_input}, 0 /* 0 means from host*/)); 
     q.finish();
     htod = clock() - htod;
-   
-    /*STARTING KERNEL(S)*/
-    std::cout << "STARTING KERNEL(S)" << std::endl;
+
+      /*STARTING KERNEL(S)*/
+    std::cout << "STARTING KERNEL - 1(S)" << std::endl;
     comp = clock();
     OCL_CHECK(err, err = q.enqueueTask(krnl1));
     q.finish();
     comp = clock() - comp;
-    std::cout << "KERNEL(S) FINISHED" << std::endl;
+    std::cout << "KERNEL - 1(S) FINISHED" << std::endl;
 
-
-    /*DEVICE -> HOST DATA TRANSFER*/
-    std::cout << "HOST <- DEVICE" << std::endl;
+     /*DEVICE -> HOST DATA TRANSFER*/
+    std::cout << "HOST <- DEVICE [Kernel-1]" << std::endl;
     dtoh = clock();
     OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output}, CL_MIGRATE_MEM_OBJECT_HOST));
     q.finish();
     dtoh = clock() - dtoh;
+    std::cout<<"\n==================================================================================================\n";
+    
+    
+    // std::cout << "HOST -> DEVICE [Kernel-2]" << std::endl;
+    // htod_opti = clock();
+    // OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_input_optimized}, 0 /* 0 means from host*/)); 
+    // q.finish();
+    // htod_opti = clock() - htod_opti;
+  
 
+    // std::cout << "STARTING KERNEL - 2(S)" << std::endl;
+    // comp_opti = clock();
+    // OCL_CHECK(err, err = q.enqueueTask(krnl2));
+    // q.finish();
+    // comp_opti = clock() - comp_opti;
+    // std::cout << "KERNEL - 2(S) FINISHED" << std::endl;
+
+    
+    // std::cout << "HOST <- DEVICE [Kernel-2]" << std::endl;
+    // dtoh_opti = clock();
+    // OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output_optimized}, CL_MIGRATE_MEM_OBJECT_HOST));
+    // q.finish();
+    // dtoh_opti = clock() - dtoh_opti;
 
     /*====================================================VERIFICATION & TIMING===============================================================*/
 
-    printf("Host -> Device : %lf ms\n", 1000.0 * htod/CLOCKS_PER_SEC);
-    printf("Device -> Host : %lf ms\n", 1000.0 * dtoh/CLOCKS_PER_SEC);
-    printf("Computation : %lf ms\n",  1000.0 * comp/CLOCKS_PER_SEC);
+    //printf("Host -> Device : %lf ms\n", 1000.0 * htod/CLOCKS_PER_SEC);
+    printf("Computation without optimization : %lf ms\n",  1000.0 * comp/CLOCKS_PER_SEC);
+    //printf("Computation with optimization : %lf ms\n",  1000.0 * comp_opti/CLOCKS_PER_SEC);
+    //printf("Device -> Host : %lf ms\n", 1000.0 * dtoh/CLOCKS_PER_SEC);
+    //printf("==========================To check:-================================\n");
+    
+
+
+
     std::cout << std::endl; 
    
     bool match = true;
 
     std::cout << output_hw[0] << std::endl;
+    //std::cout << output_hw_opti[0] << std::endl;
 
     std::cout << std::endl << "TEST " << (match ? "PASSED" : "FAILED") << std::endl;
 
--- src/krnl.cpp
+++ src/krnl.cpp
@@ -16,11 +16,27 @@ double mean(double a[])
 
 void dot_multiply(cmplx_type a[DATA_SIZE], cmplx_type b[DATA_SIZE])
 {
+   
     for (int i = 0; i < DATA_SIZE; i++) {
-        cmplx_type a_conj; 
+        cmplx_type a_conj;  //
         CMPXCONJ(a_conj, a[i]);
         CMPXMUL(b[i], a[i], a_conj);
     }
+    
+}
+void dot_multiply_optimized(cmplx_type a[DATA_SIZE], cmplx_type b[DATA_SIZE])
+{
+   
+    for (int i = 0; i < DATA_SIZE; i++) {
+        #pragma HLS unroll //factor=2 skip_exit_check off=true
+        cmplx_type a_conj;  //
+        CMPXCONJ(a_conj, a[i]);
+        CMPXMUL(b[i], a[i], a_conj);
+                    
+        
+    }
+   
+
 }
 
 cmplx_type cmpxdiv(cmplx_type a, cmplx_type b) {
@@ -37,16 +53,51 @@ void co_autocorrs(double y[DATA_SIZE], double z[DATA_SIZE])
     std::cout << "AUTO COR" << std::endl; 
     double m, nFFT;
     m = mean(y);
-    
-    cmplx_type input[DATA_SIZE];
+    //NFFT is not defined
+    cmplx_type input[DATA_SIZE]; //F
     cmplx_type output[DATA_SIZE];
     
     for (int i = 0; i < DATA_SIZE; i++) {
+        
         input[i].real = y[i] - m;
         input[i].imag = 0.0; 
     }
     pease_fft(input, output);
     dot_multiply(output, input);
+    pease_fft(input, output);
+
+    cmplx_type divisor = output[0];
+    
+    for (int i = 0; i < DATA_SIZE; i++) {
+        
+        input[i] = cmpxdiv(divisor, output[i]); // F[i] / divisor;
+    }
+
+
+    for (int i = 0; i < DATA_SIZE; i++) {
+        
+        z[i] = input[i].real;
+    }
+
+}
+void co_autocorrs_optimized(double y[DATA_SIZE], double z[DATA_SIZE])
+{   
+    std::cout << "AUTO COR" << std::endl; 
+    double m, nFFT;
+    m = mean(y);
+    //NFFT is not defined
+    cmplx_type input[DATA_SIZE]; //F
+    cmplx_type output[DATA_SIZE];
+    
+   
+    for (int i = 0; i < DATA_SIZE; i++) {
+        
+        input[i].real = y[i] - m;
+        input[i].imag = 0.0; 
+    }
+    
+    pease_fft(input, output);
+    dot_multiply_optimized(output, input);
     pease_fft(input, output);
 
     cmplx_type divisor = output[0];
@@ -64,11 +115,18 @@ void co_autocorrs(double y[DATA_SIZE], double z[DATA_SIZE])
 }
 
 
-int CO_FirstMin_ac(double window[DATA_SIZE])
+int CO_FirstMin_ac(double window[DATA_SIZE],bool optimized)
 {
     // Removed NaN check
     double autocorrs[DATA_SIZE];
-    co_autocorrs(window, autocorrs);
+    if (!optimized)
+    {
+        co_autocorrs(window, autocorrs);
+    }
+    else
+    {
+        co_autocorrs_optimized(window, autocorrs);
+    }
     int minInd = DATA_SIZE;
     for(int i = 1; i < DATA_SIZE-1; i++)
     {
@@ -82,8 +140,22 @@ int CO_FirstMin_ac(double window[DATA_SIZE])
     
 }
 
-extern "C" void krnl(data_t* input, data_t* output) {
-
+extern "C" void krnl(data_t* input, data_t* output,bool optimized) {
+    int arr_len = sizeof(input)/sizeof(input[0]);
+    std::cout <<"The length of the array is"<<arr_len;
+    // std::cout <<"==================================================================================";
+    // for (int i = 0; i < DATA_SIZE; i++) 
+    // {
+    // std::cout << input[i] << "\n";
+    // }
+    // std::cout <<"==================================================================================";
+    
+    // std::cout <<"====================================second round==============================================";
+    // for (int i = 0; i < DATA_SIZE; i++) 
+    // {
+    // std::cout << input[i] << "\n";
+    // }
+    // std::cout <<"====================================second round==============================================";
     #pragma HLS INTERFACE mode=m_axi port=input
     #pragma HLS INTERFACE mode=m_axi port=output
 
@@ -95,10 +167,12 @@ extern "C" void krnl(data_t* input, data_t* output) {
     for (int i = 0; i < DATA_SIZE; i++) {
         window[i] = input[w + i];
     }
+
+   
     
     /* Feature Extraction */
-    result = CO_FirstMin_ac(window);
-    std::cout << result; 
+    result = CO_FirstMin_ac(window,optimized);
+    //std::cout <<  << "\n"; result
     /* Writing to DDR */
     output[w] = result;
 
